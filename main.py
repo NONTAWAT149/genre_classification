23a195a795fc28eafe3f67f9e03d72518afbabf3
NONTAWAT149 <nontawat149@gmail.com>
2022-07-01 22:33:14 +0100

    revise file name

--- main.py
+++ main.py
@@ -70,7 +70,7 @@ def go(config: DictConfig):
             os.path.join(root_path, "segregate"),
             "main",
             parameters={
-                "input_artifact": "processed_data.csv:latest",
+                "input_artifact": "preprocessed_data.csv:latest",
                 "artifact_root": "data",
                 "artifact_type": "segregated_data",
                 "test_size": config["data"]["ks_alpha"],
